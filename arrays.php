ed76912ff210ffdbb1925fe265ab04b3fffb08ab
Lars Lillo Ulvestad <lars.lillo@gmail.com>
2018-03-07 21:11:07 +0100

    Arrays

---
+++ arrays.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+</head>
+<body>
+
+<?php 
+
+$numberList = array(267,8765,345,'5345', 345,
+'<h1>Hello</h1>');
+
+/* print_r($numberList); */
+
+echo $numberList[5];
+
+
+?>
+
+
+  
+</body>
+</html>
