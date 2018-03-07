ef33c8dc89eb6f5616dd2c2590db6b327f70018d
Lars Lillo Ulvestad <lars.lillo@gmail.com>
2018-03-07 19:01:29 +0100

    Adding math and variables

---
+++ math.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+</head>
+<body>
+
+<?php
+
+echo 56 + 45;
+echo "<br>";
+echo 56 - 45;
+echo "<br>";
+echo 56 * 45;
+echo "<br>";
+echo 56 / 45;
+echo "<br>";
+echo "<br>";
+
+
+
+echo 45 + 34 * 45 - 45;
+echo "<br>";
+echo 5 + 5 / 10;
+
+echo "<br>";
+
+
+$number1 = 12;
+$number2 = 24;
+
+echo $number1 * $number2;
+
+?>
+
+
+  
+</body>
+</html>
---
+++ variables.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+</head>
+<body>
+
+
+<?php 
+
+$name = 'Edwin';
+$number = 100;
+$Number_List = 100.5;
+// $num-ber= 400;
+//$0number = 500;
+$NUMBER = 100;
+$NUMBeR = '100';
+echo $name . " " . $NUMBeR;
+
+$name = "<h1> HELLO </h1>";
+
+echo $name;
+
+?>
+
+  
+</body>
+</html>
