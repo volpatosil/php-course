22520b034a8a4307d755e3f968318be3be49d094
Lars Lillo Ulvestad <lars.lillo@gmail.com>
2018-03-11 13:03:42 +0100

    CMS: Front End and First Steps Finished

--- cms/includes/db.php
+++ cms/includes/db.php
@@ -11,10 +11,10 @@ foreach($db as $key => $value) {
 
 $connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
-if($connection) {
+/* if($connection) {
 
   echo "We are connected";
 
-}
+} */
 
 ?>
--- cms/search.php
+++ cms/search.php
@@ -12,10 +12,26 @@
             <!-- Blog Entries Column -->
             <div class="col-md-8">
               <?php
-                $query = "SELECT * FROM posts";
-                $select_all_posts_query = mysqli_query($connection, $query);
 
-                  while($row = mysqli_fetch_assoc($select_all_posts_query)) {
+          
+
+            if(isset($_POST['submit'])) {
+             $search = $_POST['search'];
+
+             $query = "SELECT * FROM posts WHERE post_tags LIKE '%$search%' ";
+             $search_query = mysqli_query($connection, $query);
+
+             if(!$search_query) {
+              die("QUERY FAILED" . mysqli_error($connection));
+             }
+
+            $count = mysqli_num_rows($search_query);
+
+            if($count == 0) {
+              echo "<h1>NO RESULT</h1>";
+            } else {
+
+                  while($row = mysqli_fetch_assoc($search_query)) {
                     $post_title = $row['post_title'];
                     $post_author = $row['post_author'];
                     $post_date = $row['post_date'];
@@ -43,8 +59,10 @@
 
                 <hr>        
               <?php 
-              }
+              }}}
               ?>
+
+
 
             </div>
 
