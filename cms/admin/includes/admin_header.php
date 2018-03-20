02cb059aa37bcfdbb40e5c2d989d57182e1f122b
Lars Lillo Ulvestad <lars.lillo@gmail.com>
2018-03-20 05:43:23 +0100

    Bootstrap Modal Delete Confirm Feature Finished

--- cms/admin/includes/admin_header.php
+++ cms/admin/includes/admin_header.php
@@ -44,6 +44,7 @@ if(!isset($_SESSION['user_role'])) {
         <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
     <link href="css/styles.css" rel="stylesheet">
+    <script src="js/jquery.js"></script>
 <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
 </head>
 
