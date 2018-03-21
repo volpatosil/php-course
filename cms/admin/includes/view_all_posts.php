f2c8ca22c140fceac3007ce3e326c33fb4c9f518
Lars Lillo Ulvestad <lars.lillo@gmail.com>
2018-03-21 18:21:48 +0100

    New registration system

--- cms/admin/includes/view_all_posts.php
+++ cms/admin/includes/view_all_posts.php
@@ -180,10 +180,22 @@ if(isset($_POST['checkBoxArray'])) {
 
 
         echo "<td>{$post_date}</td>";
-        echo "<td><a href='../post.php?p_id={$post_id}'>View Post</a></td>";
-        echo "<td><a href='posts.php?source=edit_post&p_id={$post_id}'>Edit</a></td>";
+        echo "<td><a class='btn btn-primary' href='../post.php?p_id={$post_id}'>View Post</a></td>";
+        echo "<td><a class='btn btn-info' href='posts.php?source=edit_post&p_id={$post_id}'>Edit</a></td>";
 /*         echo "<td><a href='posts.php?delete={$post_id}'>Delete</a></td>"; */
-        echo "<td><a href='javascript:void(0)' rel='{$post_id}'' class='delete_link'>Delete</a></td>";
+/*         echo "<td><a href='javascript:void(0)' rel='{$post_id}'' class='delete_link'>Delete</a></td>"; */
+        ?>
+
+      <form method="post">
+        <input type="hidden" name="post_id" value="<?php echo $post_id ?>">
+
+      <?php
+
+      echo '<td><input class="btn btn-danger" type="submit" name="delete" value="Delete"></td>';
+
+      ?>
+      </form>
+      <?php
         echo "<td><a href='posts.php?reset={$post_id}'>{$post_views_count}</td>";
         echo "</tr>";
 
@@ -204,36 +216,33 @@ if(isset($_POST['checkBoxArray'])) {
 
 <?php 
           
-          if(isset($_GET['reset'])) {
-            $the_post_id = escape($_GET['reset']);
-            $query = "UPDATE posts SET post_views_count = 0 WHERE post_id =" . mysqli_real_escape_string($connection, $_GET['reset']) . " ";
-            $delete_query = mysqli_query($connection, $query);
-            header("Location: posts.php");
-          }
+  if(isset($_GET['reset'])) {
+    $the_post_id = escape($_GET['reset']);
+    $query = "UPDATE posts SET post_views_count = 0 WHERE post_id =" . mysqli_real_escape_string($connection, $_GET['reset']) . " ";
+    $delete_query = mysqli_query($connection, $query);
+    header("Location: posts.php");
+  }
 
-          if(isset($_GET['delete'])) {
-            $the_post_id = escape($_GET['delete']);
-            $query = "DELETE FROM posts WHERE post_id = {$the_post_id} ";
-            $delete_query = mysqli_query($connection, $query);
-            header("Location: posts.php");
-          }
+  if(isset($_POST['delete'])) {
+    $the_post_id = escape($_POST['post_id']);
+    $query = "DELETE FROM posts WHERE post_id = {$the_post_id} ";
+    $delete_query = mysqli_query($connection, $query);
+    header("Location: posts.php");
+  }
+
+?>
+
+<script>
           
+$(document).ready(function() {
+  $(".delete_link").on('click', function() {
+    var id = $(this).attr('rel');
+    var delete_url = 'posts.php?delete=' + id + " ";
+    
+    $('.modal_delete_link').attr("href", delete_url);
+    
+    $('#myModal').modal('show');
+  })
+});
 
-
-
-          ?>
-
-          <script>
-          
-          $(document).ready(function() {
-            $(".delete_link").on('click', function() {
-              var id = $(this).attr('rel');
-              var delete_url = 'posts.php?delete=' + id + " ";
-              
-              $('.modal_delete_link').attr("href", delete_url);
-              
-              $('#myModal').modal('show');
-            })
-          });
-
-          </script>
+</script>
--- cms/admin/includes/view_all_users.php
+++ cms/admin/includes/view_all_users.php
@@ -68,7 +68,19 @@
       echo "<td><a href='users.php?change_to_admin={$user_id}'>Admin</a></td>";
       echo "<td><a href='users.php?change_to_sub={$user_id}'>Subscriber</a></td>";
       echo "<td><a href='users.php?source=edit_user&edit_user={$user_id}'>Edit</a></td>";
-      echo "<td><a href='users.php?delete={$user_id}'>Delete</a></td>";
+      ?>
+
+      <form method="post">
+        <input type="hidden" name="user_id" value="<?php echo $user_id ?>">
+
+      <?php
+
+        echo '<td><input type="submit" name="delete">Delete</td>';
+
+        ?>
+      </form>
+      <?php
+      /* echo "<td><a href='users.php?delete={$user_id}'>Delete</a></td>"; */
       echo "</tr>";
   
 }
